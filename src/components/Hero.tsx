0e9c74d33a50c57b5dda0c4f19917f64dede7b55
puneethkumar01 <puneethkumar1405@gmail.com>
2026-02-18 15:54:17 +0530

    minor fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-scroll";
 import { ChevronDown, Trophy, CalendarDays, MapPin } from "lucide-react";
 import bgLandscape from "../assets/background_landscape.png";
 import bgPortrait from "../assets/background_potrait.png";
+import logo from "../assets/logo.png";
 
 const container = {
   hidden: { opacity: 0 },
@@ -58,7 +59,7 @@ const Hero = () => {
           className="mb-4 md:mb-6 flex justify-center"
         >
           <img
-            src="src\assets\logo.png"
+            src={logo}
             alt="Techotsav 2026 Logo"
             className="h-32 sm:h-40 md:h-48 lg:h-56 w-auto"
           />
